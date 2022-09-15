8156d734745dcae2267019d98ff101513ff1e699
GenisMon <genis.monsalve164@pascualbravo.edu.co>
2022-09-15 10:42:10 -0500

    Se elimino libreria NachoOperaSyR

--- Integrado/Program.cs
+++ Integrado/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NachoOperaSyR;
-using NachoOperaMyD;
 
 namespace Integrado
 {
@@ -12,9 +10,7 @@ namespace Integrado
     {
         static void Main(string[] args)
         {
-            long resulS, resulR, resulM, resulD;
-            resulS = NachoOperaSyR.Class1.Suma(3, 4);
-            resulR = NachoOperaSyR.Class1.Suma(10, 2);
+            long resulM, resulD;
             resulM = NachoOperaMyD.Class1.Multiplicacion(3,4);
             resulD = NachoOperaMyD.Class1.Division(10, 2);
             Console.WriteLine(resulM.ToString());
